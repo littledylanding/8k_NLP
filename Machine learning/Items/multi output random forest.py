3d79e3216c7520829f8665c163e222662b8363d8
jd2269 <dylanjiaqi@gmail.com>
2023-11-02 17:13:38 -0400

    update

--- Machine learning/Items/multi output random forest.py
+++ Machine learning/Items/multi output random forest.py
@@ -9,13 +9,12 @@ def drop_first_last(group):
     return group.iloc[1:-1]
 
 
-df = pd.read_csv("Date.csv")
-df['Filing Date'] = pd.to_datetime(df['Filing Date'])
-df = df.sort_values(by=['Ticker', 'Filing Date'])
-result = df.groupby(['Ticker', 'Filing Date']).apply(drop_first_last)
+df = pd.read_csv("Data.csv")
+df = df.sort_values(by=['Ticker'])
+result = df.groupby(['Ticker']).apply(drop_first_last)
 df = result.reset_index(drop=True)
 x_col = list(df.columns)
-err = ['Ticker', 'Filing Date', 'y']
+err = ['Ticker', 'Market', 'y', 'Ret', 'Items']
 for i in err:
     x_col.remove(i)
 y = df['y']
@@ -23,14 +22,10 @@ X = df[x_col]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=19)
 
-rf = RandomForestClassifier(n_estimators=1000)
+rf = RandomForestClassifier(n_estimators=1000, random_state=19)
 
-multi_target_rf = MultiOutputClassifier(rf, n_jobs=-1)
+rf.fit(X_train, y_train)
 
-multi_target_rf.fit(X_train, y_train)
-
-y_pred = multi_target_rf.predict(X_test)
-for i, col in enumerate(y.columns):
-    print(f"Classification Report for {col}:\n")
-    print(classification_report(y_test.iloc[:, i], y_pred[:, i]))
-    print("\n")
+y_pred = rf.predict(X_test)
+print("Classification Report:\n")
+print(classification_report(y_test, y_pred))
